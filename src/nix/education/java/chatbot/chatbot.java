b616eacb97f83b6054a882cab9882c2e547e4cbc
Игорь Зубатый <i.v.zubatyi@student.khai.edu>
2021-11-15 12:44:50 +0200

    ChatBot 2-nd stage

--- src/nix/education/java/chatbot/chatbot.java
+++ src/nix/education/java/chatbot/chatbot.java
@@ -7,6 +7,10 @@ public class chatbot {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, dude! My name is Abobus");
         System.out.println("I was created in 2021");
+        System.out.println("Please, remind me your name");
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have," + name);
+
     }
 
 }
