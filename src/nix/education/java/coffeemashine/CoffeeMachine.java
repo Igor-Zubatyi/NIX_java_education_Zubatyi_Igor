72604db262ffcde3428acbd31d97fb0aa75cb346
Игорь Зубатый <i.v.zubatyi@student.khai.edu>
2021-11-15 18:48:15 +0200

    Coffee Machine Project - Stage 3

--- src/nix/education/java/coffeemashine/CoffeeMachine.java
+++ src/nix/education/java/coffeemashine/CoffeeMachine.java
@@ -7,8 +7,9 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         makingCoffee();
         calculationOfIngredients();
+        checkingOfIngredients();
     }
-    public static void makingCoffee() {
+     static void makingCoffee() {
         System.out.println("Starting to make a coffee");
         System.out.println("Grinding coffee beans");
         System.out.println("Boiling water");
@@ -17,7 +18,7 @@ public class CoffeeMachine {
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready");
     }
-    public static void calculationOfIngredients(){
+     static void calculationOfIngredients(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Write how many cups of coffee you will need:");
         int numberOfCups = scanner.nextInt();
@@ -28,5 +29,35 @@ public class CoffeeMachine {
         System.out.println(numberOfWater + " ml of water");
         System.out.println(numberOfMilk + " ml of milk");
         System.out.println(numberOfCoffeeBeans + " g of coffee beans");
+    }
+     static void checkingOfIngredients(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int numberOfWater = scanner.nextInt();
+         System.out.println("Write how many ml of milk the coffee machine has:");
+        int numberOfMilk = scanner.nextInt();
+         System.out.println("Write how many grams of coffee beans the coffee machine has:");
+         int numberOfCoffeeBeans = scanner.nextInt();
+         System.out.println("Write how many cups of coffee you need:");
+         int numberOfCups = scanner.nextInt();
+         if(numberOfWater < numberOfCups * 200 || numberOfMilk < numberOfCups * 50 || numberOfCoffeeBeans < numberOfCups * 15){
+             while (numberOfWater < numberOfCups * 200 || numberOfMilk < numberOfCups * 50 || numberOfCoffeeBeans < numberOfCups * 15){
+                 numberOfCups--;
+             }
+             System.out.println("No, I can make only "+ numberOfCups + " cups of coffee");
+         }
+         else if((numberOfWater >= numberOfCups * 200 && numberOfWater < (numberOfCups + 1) * 200) ||
+                 (numberOfMilk >= numberOfCups * 50 && numberOfMilk < (numberOfCups + 1) * 50) ||
+                 (numberOfCoffeeBeans >= numberOfCups * 15 && numberOfCoffeeBeans < (numberOfCups + 1) * 15)){
+             System.out.println("Yes, I can make that amount of coffee");
+         }
+         else {
+             int N = 0;
+             do{
+                 N++;
+                 numberOfCups +=N;
+             }while (numberOfCups * 200 < numberOfWater && numberOfCups * 50 < numberOfMilk && numberOfCups * 15 < numberOfCoffeeBeans);
+             System.out.println("Yes, I can make that amount of coffee (and even " + N + " more of that)");
+         }
     }
 }
