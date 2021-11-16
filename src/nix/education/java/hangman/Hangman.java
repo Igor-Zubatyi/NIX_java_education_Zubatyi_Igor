ce3e0200e187ba164bca9313b1ebf1873c488f47
Игорь Зубатый <i.v.zubatyi@student.khai.edu>
2021-11-17 01:30:52 +0200

    Hangman Project - Stage 2

--- src/nix/education/java/hangman/Hangman.java
+++ src/nix/education/java/hangman/Hangman.java
@@ -1,11 +1,24 @@
 package nix.education.java.hangman;
 
+import java.util.Scanner;
+
 public class Hangman {
     public static void main(String[] args) {
         welcome();
+        result(attempt());
     }
     static void welcome(){
         System.out.println("HANGMAN");
-        System.out.println("The game will be available soon.");
+        System.out.println("Guess the world");
+    }
+    static String attempt(){
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        return word;
+    }
+    static void result (String word){
+        if (word.equalsIgnoreCase("java"))
+            System.out.println("You survived!");
+        else System.out.println("You lost");
     }
 }
