786bc98d84aab9ec9d4dba83b1158f1400f90ce3
Игорь Зубатый <i.v.zubatyi@student.khai.edu>
2021-11-17 01:22:09 +0200

    Hangman Project - Stage 1

--- src/nix/education/java/hangman/Hangman.java
+++ src/nix/education/java/hangman/Hangman.java
@@ -1,2 +1,11 @@
-package nix.education.java.hangman;public class Hangman {
+package nix.education.java.hangman;
+
+public class Hangman {
+    public static void main(String[] args) {
+        welcome();
+    }
+    static void welcome(){
+        System.out.println("HANGMAN");
+        System.out.println("The game will be available soon.");
+    }
 }
