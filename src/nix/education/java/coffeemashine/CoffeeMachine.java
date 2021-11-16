d941f9118c654c7d433fa84a176d4d2cdfc07359
Игорь Зубатый <i.v.zubatyi@student.khai.edu>
2021-11-16 19:13:09 +0200

    Coffee Machine Progect - Stage 5

--- src/nix/education/java/coffeemashine/CoffeeMachine.java
+++ src/nix/education/java/coffeemashine/CoffeeMachine.java
@@ -5,77 +5,28 @@ import java.util.Scanner;
 public class CoffeeMachine {
 
     public static void main(String[] args) {
-        makingCoffee();
-        calculationOfIngredients();
-        checkingOfIngredients();
         coffeeMachineAction();
-    }
-     static void makingCoffee() {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready");
-    }
-     static void calculationOfIngredients(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Write how many cups of coffee you will need:");
-        int numberOfCups = scanner.nextInt();
-        int numberOfWater = numberOfCups * 200;
-        int numberOfMilk = numberOfCups * 50;
-        int numberOfCoffeeBeans = numberOfCups * 15;
-        System.out.println("For " + numberOfCups + " cups of coffee you will need:");
-        System.out.println(numberOfWater + " ml of water");
-        System.out.println(numberOfMilk + " ml of milk");
-        System.out.println(numberOfCoffeeBeans + " g of coffee beans");
-    }
-     static void checkingOfIngredients(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int numberOfWater = scanner.nextInt();
-         System.out.println("Write how many ml of milk the coffee machine has:");
-        int numberOfMilk = scanner.nextInt();
-         System.out.println("Write how many grams of coffee beans the coffee machine has:");
-         int numberOfCoffeeBeans = scanner.nextInt();
-         System.out.println("Write how many cups of coffee you need:");
-         int numberOfCups = scanner.nextInt();
-         if(numberOfWater < numberOfCups * 200 || numberOfMilk < numberOfCups * 50 || numberOfCoffeeBeans < numberOfCups * 15){
-             while (numberOfWater < numberOfCups * 200 || numberOfMilk < numberOfCups * 50 || numberOfCoffeeBeans < numberOfCups * 15){
-                 numberOfCups--;
-             }
-             System.out.println("No, I can make only "+ numberOfCups + " cups of coffee");
-         }
-         else if((numberOfWater >= numberOfCups * 200 && numberOfWater < (numberOfCups + 1) * 200) ||
-                 (numberOfMilk >= numberOfCups * 50 && numberOfMilk < (numberOfCups + 1) * 50) ||
-                 (numberOfCoffeeBeans >= numberOfCups * 15 && numberOfCoffeeBeans < (numberOfCups + 1) * 15)){
-             System.out.println("Yes, I can make that amount of coffee");
-         }
-         else {
-             int N = 0;
-             do{
-                 N++;
-                 numberOfCups +=N;
-             }while (numberOfCups * 200 < numberOfWater && numberOfCups * 50 < numberOfMilk && numberOfCups * 15 < numberOfCoffeeBeans);
-             System.out.println("Yes, I can make that amount of coffee (and even " + N + " more of that)");
-         }
     }
     static void coffeeMachineAction(){
         CoffeeMachineRealization coffeeMachineRealization = new CoffeeMachineRealization();
-        coffeeMachineRealization.productsInformation();
-        System.out.println("Write action (buy, fill, take):");
-        Scanner scanner = new Scanner(System.in);
-        String point = scanner.nextLine();
-        if (point.equalsIgnoreCase("buy"))
-            coffeeMachineRealization.buy();
-        else if (point.equalsIgnoreCase("fill"))
-            coffeeMachineRealization.fill();
-        else if (point.equalsIgnoreCase("take"))
-            coffeeMachineRealization.take();
-        else
-            System.out.println("Wrong action");
-
+        boolean working = true;
+        while (working == true) {
+            System.out.println("Write action (buy, fill, take, remaining, exit):");
+            Scanner scanner = new Scanner(System.in);
+            String point = scanner.nextLine();
+            if (point.equalsIgnoreCase("buy"))
+                coffeeMachineRealization.buy();
+            else if (point.equalsIgnoreCase("fill"))
+                coffeeMachineRealization.fill();
+            else if (point.equalsIgnoreCase("take"))
+                coffeeMachineRealization.take();
+            else if (point.equalsIgnoreCase("exit"))
+                working = false;
+            else if (point.equalsIgnoreCase("remaining"))
+                coffeeMachineRealization.productsInformation();
+            else
+                System.out.println("Wrong action");
+        }
     }
 }
 class CoffeeMachineRealization{
@@ -124,30 +75,46 @@ class CoffeeMachineRealization{
         System.out.println(getNumberOfMoney() + " of money");
     }
     void buy() {
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, 4 - back to main menu:");
         Scanner scanner = new Scanner(System.in);
         int point = scanner.nextInt();
         if (point == 1) {
-            setNumberOfWater(getNumberOfWater() - 250);
-            setNumberOfCoffeeBeans(getNumberOfCoffeeBeans() - 16);
-            setNumberOfMoney(getNumberOfCoffeeBeans() + 4);
-            setNumberOfGlasses(getNumberOfGlasses() - 1);
+            if(getNumberOfWater() - 250 >= 0 && getNumberOfCoffeeBeans() - 16 >= 0 && getNumberOfGlasses() > 0) {
+                System.out.println("I have enough resources, making you a coffee!");
+                setNumberOfWater(getNumberOfWater() - 250);
+                setNumberOfCoffeeBeans(getNumberOfCoffeeBeans() - 16);
+                setNumberOfMoney(getNumberOfMoney() + 4);
+                setNumberOfGlasses(getNumberOfGlasses() - 1);
+            }
+            else System.out.println("I don`t have enough resources");
         } else if (point == 2) {
-            setNumberOfWater(getNumberOfWater() - 350);
-            setNumberOfMilk(getNumberOfMilk() - 75);
-            setNumberOfCoffeeBeans(getNumberOfCoffeeBeans() - 20);
-            setNumberOfMoney(getNumberOfMoney() + 7);
-            setNumberOfGlasses(getNumberOfGlasses() - 1);
+            if (getNumberOfWater() - 350 >= 0 && getNumberOfMilk() - 75 >= 0 && getNumberOfCoffeeBeans() - 20 >= 0 && getNumberOfGlasses() > 0) {
+                System.out.println("I have enough resources, making you a coffee!");
+                setNumberOfWater(getNumberOfWater() - 350);
+                setNumberOfMilk(getNumberOfMilk() - 75);
+                setNumberOfCoffeeBeans(getNumberOfCoffeeBeans() - 20);
+                setNumberOfMoney(getNumberOfMoney() + 7);
+                setNumberOfGlasses(getNumberOfGlasses() - 1);
+
+            } else {
+                System.out.println("I don`t have enough resources");
+            }
         } else if (point == 3){
-            setNumberOfWater( getNumberOfWater() - 200);
-            setNumberOfMilk(getNumberOfMilk() - 100);
-            setNumberOfCoffeeBeans(getNumberOfCoffeeBeans() - 12);
-            setNumberOfMoney(getNumberOfMoney() + 6);
-            setNumberOfGlasses(getNumberOfGlasses() - 1);
+            if (getNumberOfWater() - 200 >= 0 && getNumberOfMilk() - 100 >= 0 && getNumberOfCoffeeBeans() - 12 >= 0 && getNumberOfGlasses() > 0) {
+                System.out.println("I have enough resources, making you a coffee!");
+                setNumberOfWater( getNumberOfWater() - 200);
+                setNumberOfMilk(getNumberOfMilk() - 100);
+                setNumberOfCoffeeBeans(getNumberOfCoffeeBeans() - 12);
+                setNumberOfMoney(getNumberOfMoney() + 6);
+                setNumberOfGlasses(getNumberOfGlasses() - 1);
+            } else {
+                System.out.println("I don`t have enough resources");
+            }
         }
+        else if (point == 4)
+            CoffeeMachine.coffeeMachineAction();
         else
             System.out.println("Wrong number");
-        productsInformation();
     }
     void fill(){
         Scanner scanner = new Scanner(System.in);
@@ -166,7 +133,8 @@ class CoffeeMachineRealization{
         productsInformation();
     }
     void take(){
-     setNumberOfMoney(0);
-     productsInformation();
+        System.out.println("I gave you " + getNumberOfMoney());
+        setNumberOfMoney(0);
+
     }
 }
