7f1018b88fc20be333441f2a93f045b09459dc29
Игорь Зубатый <i.v.zubatyi@student.khai.edu>
2021-11-18 01:20:32 +0200

    HangMan Project - Stage 5

--- src/nix/education/java/hangman/Hangman.java
+++ src/nix/education/java/hangman/Hangman.java
@@ -5,36 +5,67 @@ import java.util.Scanner;
 
 public class Hangman {
     static String randomWord = randomWord();
+    static int attempts = 8;
+
     public static void main(String[] args) {
         welcome();
-        result(attempt());
+        result();
     }
-    static void welcome(){
-        System.out.println("HANGMAN");
-        System.out.println("Guess the world");
-        System.out.println(hint(randomWord));
-    }
-    static String attempt(){
-        Scanner scanner = new Scanner(System.in);
-        String word = scanner.nextLine();
-        return word;
-    }
-    static void result (String word){
 
-        if (word.equalsIgnoreCase(randomWord))
-            System.out.println("You survived!");
-        else System.out.println("You lost " + randomWord);
+    static void welcome() {
+        System.out.println("HANGMAN");
     }
-    static String randomWord(){
+
+    static char attempt() {
+        Scanner scanner = new Scanner(System.in);
+        String letter = scanner.nextLine();
+        char[] letters = letter.toCharArray();
+        return letters[0];
+    }
+
+    static void result() {
+        StringBuilder hiddenWord = new StringBuilder(randomWord);
+        StringBuilder guessWord = new StringBuilder();
+        for (int i = 0; i < randomWord.length(); i++) {
+            guessWord.append("-");
+        }
+        while (attempts > 0) {
+            System.out.println("Input letter");
+            System.out.println(guessWord);
+            char nextLetter = attempt();
+
+            int indexOfLetter = hiddenWord.indexOf("" + nextLetter);
+            if (indexOfLetter != -1) {
+                while (indexOfLetter != -1) {
+                    hiddenWord.setCharAt(indexOfLetter, '-');
+                    guessWord.setCharAt(indexOfLetter, randomWord.charAt(indexOfLetter));
+                    if (randomWord.equals(guessWord.toString())) {
+                        System.out.println("You survived");
+                        System.out.println(guessWord);
+                        attempts = 0;
+                    }
+                    indexOfLetter = hiddenWord.indexOf("" + nextLetter);
+                }
+            } else {
+                System.out.println("That letter doesn't appear in the word\n");
+            }
+            attempts--;
+        }
+        System.out.println("Thanks for playing!\n" +
+                "We'll see how well you did in the next stage\n");
+    }
+
+    static String randomWord() {
         Random random = new Random();
-        String[] wordsArray = new String[] {"python", "java", "javascript", "kotlin"};
+        String[] wordsArray = new String[]{"python", "java", "javascript", "kotlin"};
         String randomWord = wordsArray[random.nextInt(wordsArray.length)];
         return randomWord;
     }
-    static String hint(String randomWord){
+
+    static String hint(String randomWord) {
         char[] letters = randomWord.toCharArray();
         for (int i = 2; i < letters.length; i++) {
-            letters [i] = '-';
+            letters[i] = '-';
         }
         String word = new String(letters);
         return word;
