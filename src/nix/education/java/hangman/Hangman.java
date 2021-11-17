ae96deceb201dbda2a7e9c5578eb9a3977109414
Игорь Зубатый <i.v.zubatyi@student.khai.edu>
2021-11-17 11:40:12 +0200

    Hangman Project - Stage 4

--- src/nix/education/java/hangman/Hangman.java
+++ src/nix/education/java/hangman/Hangman.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
+    static String randomWord = randomWord();
     public static void main(String[] args) {
         welcome();
         result(attempt());
@@ -11,6 +12,7 @@ public class Hangman {
     static void welcome(){
         System.out.println("HANGMAN");
         System.out.println("Guess the world");
+        System.out.println(hint(randomWord));
     }
     static String attempt(){
         Scanner scanner = new Scanner(System.in);
@@ -18,14 +20,23 @@ public class Hangman {
         return word;
     }
     static void result (String word){
-        if (word.equalsIgnoreCase(randomWord()))
+
+        if (word.equalsIgnoreCase(randomWord))
             System.out.println("You survived!");
-        else System.out.println("You lost " + randomWord());
+        else System.out.println("You lost " + randomWord);
     }
     static String randomWord(){
         Random random = new Random();
         String[] wordsArray = new String[] {"python", "java", "javascript", "kotlin"};
         String randomWord = wordsArray[random.nextInt(wordsArray.length)];
         return randomWord;
-    };
+    }
+    static String hint(String randomWord){
+        char[] letters = randomWord.toCharArray();
+        for (int i = 2; i < letters.length; i++) {
+            letters [i] = '-';
+        }
+        String word = new String(letters);
+        return word;
+    }
 }
